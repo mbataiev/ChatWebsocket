897fe6697edf8dfba86d4418d9f7f915d7ccc699
superlalkaTV <maksim.bataev.2016@gmail.com>
2023-08-10 18:58:09 +0300

    Initial Commit

---
+++ src/main/java/com/app/chat/websocket/ChatWebsocketApplication.java
@@ -0,0 +1,13 @@
+package com.app.chat.websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatWebsocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatWebsocketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/chat/websocket/ChatWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.chat.websocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatWebsocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
